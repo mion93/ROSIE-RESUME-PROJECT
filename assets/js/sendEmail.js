8211de075a456d7251a53f16788e98074a1abc27
Miriam Gurrieri <mion93@hotmail.it>
2020-06-10 10:40:56 +0000

    google maps api

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -2,10 +2,11 @@ function sendMail(contactForm) {
     emailjs.send("gmail", "rosie", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
-        "project_request": contactForm.projectsummary.value
+        "massage": contactForm.massage.value
     })
     .then(
         function(response) {
+            
             console.log("SUCCESS", response);
         },
         function(error) {
